492631853f59ebb4ce32428081afcedb37f7c364
BulletFlux <skbulletflux@gmail.com>
2022-04-12 18:36:02 +0530

    updated scripts

---
+++ ancient_mido.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export GITHUB_USER="ArhamDharewa"
+export GITHUB_EMAIL="Dharewaarham@gmail.com"
+export GITHUB_TOKEN=""
+
+export device="mido"
+
+export ROM="mido"
+export ROM_DIR="/var/lib/jenkins/workspace/ancient"
+export ROM_VERSION="12"
+export local_manifest_url=""
+export manifest_url="https://github.com/Ancient-Roms/manifest"
+export rom_vendor_name="ancient"
+export branch="twelve"
+export bacon="bacon"
+export buildtype="userdebug"
+export clean="false"
+export generate_incremental="false"
+export upload_recovery="false"
+
+export ccache="true"
+export ccache_size="100"
+
+export jenkins="true"
+
+export release_repo="ArhamDharewa/release"
+
+export timezone="UTC"
